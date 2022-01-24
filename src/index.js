11833f3a7e7fa4c1e2975214673607559c1b07e8
ametrey <aetcheverryrey@gmail.com>
2022-01-24 15:23:31 -0300

    ancedotes finished

--- src/index.js
+++ src/index.js
@@ -1,20 +1,66 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Title = ({text}) => <h1>{text}</h1>
+
+const Display = ({text}) => <p>{text}</p>
+
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const VotesWinner = ({ anecdotes, points }) => {
+
+  const mostVotedQuote = () => points.indexOf(Math.max(...points))
+
+  if ( points[mostVotedQuote()] === 0) {
+    return <p>No votes yet</p>
+  }else
+  
+  return (
+    <div>
+
+    
+    <p>{anecdotes[mostVotedQuote()]}</p>
+    <p>Has {points[mostVotedQuote()]} points.</p>
+
+    </div>
+  )
+}
 
 const App = (props) => {
 
   const [selected, setSelected] = useState(0)
 
-  const handleRandomSelect = (props) => {
+
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0))
+
+
+  const handleVote = () => {
+    const copy = [...points]
+    copy[selected]++
+    setPoints(copy);
+    console.log(copy)
+  }
+
+
+  const handleRandomSelect = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length))
   }
-  
+
+
+
+
+
   return (
     <div>
-      <div>{props.anecdotes[selected]}</div>
+      <Title text="Anecdote of the day" />
+      <Display text={anecdotes[selected]}/>
+      <Display text={'Has ' + points[selected] + ' votes'} />
       <Button onClick={handleRandomSelect} text="Get random" />
+      <Button onClick={handleVote} text="Vote" />
+      <Title text="Anecdote with most votes" />
+      <VotesWinner anecdotes={anecdotes} points={points} />
+
+
     </div>
   )
 }
